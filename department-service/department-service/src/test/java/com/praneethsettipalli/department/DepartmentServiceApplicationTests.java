e4dd7ca313e0d805397289f97e15841846e60e78
reddyse <praneeth_reddy@outlook.com>
2023-06-25 18:53:52 -0700

    Setting up project

---
+++ department-service/department-service/src/test/java/com/praneethsettipalli/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praneethsettipalli.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
