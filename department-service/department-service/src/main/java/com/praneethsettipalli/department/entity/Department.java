6bd59039ad342d1ae1b1429509ed604f36e32915
reddyse <praneeth_reddy@outlook.com>
2023-06-25 19:52:25 -0700

    Add Department entity

---
+++ department-service/department-service/src/main/java/com/praneethsettipalli/department/entity/Department.java
@@ -0,0 +1,24 @@
+package com.praneethsettipalli.department.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentcode;
+
+}
